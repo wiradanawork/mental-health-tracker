a9097a8ff69400732a22b5efbabd31bcdde94336
rayyan <wiradana.work@gmail.com>
2024-09-18 14:38:52 +0700

    tes

---
+++ main/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+import uuid  # tambahkan baris ini di paling atas
+from django.contrib.auth.models import User
+
+class MoodEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
+    mood = models.CharField(max_length=255)
+    time = models.DateField(auto_now_add=True)
+    feelings = models.TextField()
+    mood_intensity = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    @property
+    def is_mood_strong(self):
+        return self.mood_intensity > 5
---
+++ mental_health_tracker/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from main.views import show_main
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', show_main, name='show_main'),
+    path('', include('main.urls')),
+]
