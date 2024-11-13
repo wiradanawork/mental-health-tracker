79475701be4292930654b67a8766a06ed8e86107
rayyan <wiradana.work@gmail.com>
2024-11-13 15:32:59 +0700

    saya sudah selesai tutorial 8

--- main/views.py
+++ main/views.py
@@ -1,34 +1,40 @@
+from django.shortcuts import render
 from django.shortcuts import render, redirect   # Tambahkan import redirect di baris ini
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
 from django.http import HttpResponse
 from django.core import serializers
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import authenticate, login
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.shortcuts import reverse
+from django.http import HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.http import JsonResponse
+
+# Create your views here.
 
 @login_required(login_url='/login')
-def show_main(request):
-    
 
+def show_main(request):
 
     context = {
-         'name': request.user.username,
-        'class': 'PBP D',
-        'npm': '2306123456',
+        'name': request.user.username,
+        'class': 'PBP C',
+        'npm': '2306245680',
         'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
+
 
 def create_mood_entry(request):
     form = MoodEntryForm(request.POST or None)
@@ -36,7 +42,7 @@ def create_mood_entry(request):
     if form.is_valid() and request.method == "POST":
         mood_entry = form.save(commit=False)
         mood_entry.user = request.user
-        mood_entry.save()
+        form.save()
         return redirect('main:show_main')
 
     context = {'form': form}
@@ -60,11 +66,11 @@ def add_mood_entry_ajax(request):
     return HttpResponse(b"CREATED", status=201)
 
 def show_xml(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -92,14 +98,13 @@ def login_user(request):
       form = AuthenticationForm(data=request.POST)
 
       if form.is_valid():
-        user = form.get_user()
-        login(request, user)
-        response = HttpResponseRedirect(reverse("main:show_main"))
-        response.set_cookie('last_login', str(datetime.datetime.now()))
-        return response
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
       else:
-         messages.error(request, "Invalid username or password. Please try again.")
-      
+          messages.error(request, "Invalid username or password. Please try again.")
 
    else:
       form = AuthenticationForm(request)
@@ -134,4 +139,21 @@ def delete_mood(request, id):
     mood.delete()
     # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('main:show_main'))
-# Create your views here.
+
+@csrf_exempt
+def create_mood_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_mood = MoodEntry.objects.create(
+            user=request.user,
+            mood=data["mood"],
+            mood_intensity=int(data["mood_intensity"]),
+            feelings=data["feelings"]
+        )
+
+        new_mood.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
--- mental_health_tracker/urls.py
+++ mental_health_tracker/urls.py
@@ -1,10 +1,23 @@
-from django.urls import path, include
-from main.views import show_main
-from main.views import delete_mood
+"""
+URL configuration for mental_health_tracker project.
 
-app_name = 'main'
-
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
 urlpatterns = [
-    path('', show_main, name='show_main'),
-    path('', include('main.urls')),
+    path('admin/', admin.site.urls),
+    path('',include('main.urls')),
+    path('auth/', include('authentication.urls')),
 ]
